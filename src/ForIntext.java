ddbe55c9fd898516c1c9c75f4347ed050b62d29c
luanamastro <mastroianniluana7@gmail.com>
2023-05-23 20:26:36 -0300

    Se commit los ejercicios

---
+++ src/ForIntext.java
@@ -0,0 +1,10 @@
+public class ForIntext {
+    public static void main(String[] args) {
+        int contador;
+        for (contador =0; contador <=1;contador++){
+            System.out.println( "repeticion numero :" + contador);
+
+        }
+        System.out.println("el valor final :" + contador);
+    }
+}
